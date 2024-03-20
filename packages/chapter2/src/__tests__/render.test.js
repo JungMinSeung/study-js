eef46a4e579528219d6e0063d7caf137a67a564f
Min Seung Jung <alstmd94@hanyang.ac.kr>
2024-03-20 09:03:42 +0900

    test: 테스트 케이스 추가 - 하위 노드 변경, 하위 노드 변경 및 삭제

--- packages/chapter2/src/__tests__/render.test.js
+++ packages/chapter2/src/__tests__/render.test.js
@@ -86,44 +86,6 @@ describe('render > ', () => {
       expect(children[2]).not.toBe(newChildren[2]);
     });
 
-    test('props 수정', () => {
-      const $root = document.createElement('div');
-      const App = jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단')
-      );
-
-      render($root, App);
-
-      expect($root.innerHTML).toBe(
-        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
-      );
-
-      const children = [...$root.querySelectorAll('p')];
-
-      render(
-        $root,
-        jsx(
-          'div',
-          null,
-          jsx('p', null, '첫 번째 문단'),
-          jsx('p', null, '두 번째 문단')
-        ),
-        App
-      );
-
-      expect($root.innerHTML).toBe(
-        `<div><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
-      );
-
-      const newChildren = [...$root.querySelectorAll('p')];
-
-      expect(children[0]).toBe(newChildren[0]);
-      expect(children[1]).toBe(newChildren[1]);
-    });
-
     test('하위 노드 삭제', () => {
       const $root = document.createElement('div');
       const App = jsx(
@@ -162,6 +124,135 @@ describe('render > ', () => {
       expect(children[0]).toBe(newChildren[0]);
       expect(children[1]).toBe(newChildren[1]);
       expect(children[2]).not.toBe(newChildren[2]);
+    });
+
+    test('하위 노드 변경', () => {
+      const $root = document.createElement('div');
+      const App = jsx(
+        'div',
+        { id: 'test-id', class: 'test-class' },
+        jsx('p', null, '첫 번째 문단'),
+        jsx('p', null, '두 번째 문단'),
+        jsx('p', null, '세 번째 문단')
+      );
+
+      render($root, App);
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p><p>세 번째 문단</p></div>`
+      );
+
+      const children = [...$root.querySelectorAll('p')];
+
+      render(
+        $root,
+        jsx(
+          'div',
+          { id: 'test-id', class: 'test-class' },
+          jsx('p', null, '첫 번째 문단'),
+          jsx('p', null, '세 번째 문단'),
+          jsx('p', null, '두 번째 문단')
+        ),
+        App
+      );
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>세 번째 문단</p><p>두 번째 문단</p></div>`
+      );
+
+      const newChildren = [...$root.querySelectorAll('p')];
+
+      expect(children[0]).toBe(newChildren[0]);
+      expect(children[1]).toBe(newChildren[1]);
+      expect(children[2]).toBe(newChildren[2]);
+    });
+
+    test('하위 노드 변경 및 삭제', () => {
+      const $root = document.createElement('div');
+      const App = jsx(
+        'div',
+        { id: 'test-id', class: 'test-class' },
+        jsx('p', null, '첫 번째 문단'),
+        jsx('p', null, '두 번째 문단'),
+        jsx('p', null, '세 번째 문단')
+      );
+
+      render($root, App);
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p><p>세 번째 문단</p></div>`
+      );
+
+      const children = [...$root.querySelectorAll('p')];
+
+      render(
+        $root,
+        jsx(
+          'div',
+          { id: 'test-id', class: 'test-class' },
+          jsx('p', null, '첫 번째 문단'),
+          jsx('p', null, '세 번째 문단')
+        ),
+        App
+      );
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>세 번째 문단</p></div>`
+      );
+
+      const newChildren = [...$root.querySelectorAll('p')];
+
+      /**
+       * children  [ '첫 번째 문단', '세 번째 문단', '세 번째 문단' ]
+       * newChildren [ '첫 번째 문단', '세 번째 문단' ]
+       * 1. 두번째 렌더에서 children의 두 번째 요소가 newNode의 두 번째 요소로 변경되었음
+       *    즉, children의 두 번째 요소와 세 번째요소는 다른 객체를 참조하고 있음
+       * 2. 두번째 렌더에서 세번째 요소를 삭제하더라도, children의 세 번째 요소가 삭제되는건 아니다.
+       *    왜냐하면 children의 세 번째 요소에서 계속 참조하고 있음.
+       */
+
+      expect(children[0]).toBe(newChildren[0]);
+      expect(children[1]).toBe(newChildren[1]);
+      expect(children[2]).not.toBe(newChildren[2]);
+      expect(children[1]).not.toBe(children[2]);
+    });
+
+    test('props 수정', () => {
+      const $root = document.createElement('div');
+      const App = jsx(
+        'div',
+        { id: 'test-id', class: 'test-class' },
+        jsx('p', null, '첫 번째 문단'),
+        jsx('p', null, '두 번째 문단')
+      );
+
+      render($root, App);
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
+
+      const children = [...$root.querySelectorAll('p')];
+
+      render(
+        $root,
+        jsx(
+          'div',
+          null,
+          jsx('p', null, '첫 번째 문단'),
+          jsx('p', null, '두 번째 문단')
+        ),
+        App
+      );
+
+      expect($root.innerHTML).toBe(
+        `<div><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
+
+      const newChildren = [...$root.querySelectorAll('p')];
+
+      expect(children[0]).toBe(newChildren[0]);
+      expect(children[1]).toBe(newChildren[1]);
     });
   });
 });
