e05924a3ab2939bc3969b22d470660d1f7855347
Min Seung Jung <alstmd94@hanyang.ac.kr>
2024-03-21 19:53:45 +0900

    feat: useState 구현

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,27 @@
 export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
+  let state = [];
+  let stateIndex = 0;
+
+  const useState = (initialValue) => {
+    const currentIndex = stateIndex;
+    state[currentIndex] = state[currentIndex] ?? initialValue;
+
+    const setState = (newValue) => {
+      if (state[currentIndex] !== newValue) {
+        state[currentIndex] = newValue;
+        callback();
+      }
+    };
+
+    stateIndex++;
+    return [state[currentIndex], setState];
   };
 
-  const useMemo = (fn, refs) => {
-    return fn();
-  };
+  const useMemo = (fn, refs) => {};
 
   const resetContext = () => {
-
-  }
+    stateIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
