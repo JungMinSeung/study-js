ebbbe1814e67418206e48ea6989767752441cdfa
Min Seung Jung <alstmd94@hanyang.ac.kr>
2024-03-21 19:59:21 +0900

    feat: useMemo 작성

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,6 +1,8 @@
 export function createHooks(callback) {
   let state = [];
+  let memoizedValues = [];
   let stateIndex = 0;
+  let memoIndex = 0;
 
   const useState = (initialValue) => {
     const currentIndex = stateIndex;
@@ -17,11 +19,33 @@ export function createHooks(callback) {
     return [state[currentIndex], setState];
   };
 
-  const useMemo = (fn, refs) => {};
+  const useMemo = (fn, refs) => {
+    const currentIndex = memoIndex;
+    const hasRefsChanged =
+      memoizedValues[currentIndex] === undefined ||
+      !areArraysEqual(memoizedValues[currentIndex].refs, refs);
+
+    if (hasRefsChanged) {
+      memoizedValues[currentIndex] = {
+        value: fn(),
+        refs,
+      };
+    }
+    memoIndex++;
+    return memoizedValues[currentIndex].value;
+  };
 
   const resetContext = () => {
     stateIndex = 0;
+    memoIndex = 0;
   };
+
+  function areArraysEqual(arr1, arr2) {
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((value, index) => value === arr2[index])
+    );
+  }
 
   return { useState, useMemo, resetContext };
 }
