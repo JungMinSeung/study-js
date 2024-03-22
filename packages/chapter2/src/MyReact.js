ed5cde46de48571dafddcaaa7dcb997963b3f612
Min Seung Jung <alstmd94@hanyang.ac.kr>
2024-03-22 21:49:03 +0900

    feat: myreact 작성

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -1,11 +1,29 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from './hooks';
+import { render as updateElement } from './render';
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let _root;
+  let _rootComponent;
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  const _render = () => {
+    resetHookContext();
+    _rootComponent();
+
+    _root.innerHTML = null;
+    render(_root, _rootComponent);
+  };
+
+  function render($root, rootComponent) {
+    _root = $root;
+    _rootComponent = rootComponent;
+    updateElement(_root, _rootComponent());
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
